48f2d32e1843dc013e9e72049c795b7798082944
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2021-05-24 18:16:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ let plan = document.querySelector('.plan');
 
 let switchers = plan.querySelectorAll('input[name="plan"]');
 let switcherActive = plan.querySelector('input[name="plan"][checked]');
+let switcherActiveText = switcherActive.value;
 
-let currentPlan = plan.querySelector("._plan-".concat(switcherActive.value));
+let currentPlan = plan.querySelector("._plan-".concat(switcherActive.id));
 currentPlan.classList.add('plan__items-active');
 currentPlan.style.maxHeight = "".concat(currentPlan.scrollHeight, "px");
 currentPlan.style.marginTop = '40px';
@@ -20,10 +21,11 @@ var _loop5 = function _loop5(i) {
     activePlan.classList.remove('plan__items-active');
     activePlan.style.maxHeight = '';
     activePlan.style.marginTop = '';
-    var currentPlan = plan.querySelector("._plan-".concat(switchers[i].value));
+    var currentPlan = plan.querySelector("._plan-".concat(switchers[i].id));
     currentPlan.classList.add('plan__items-active');
     currentPlan.style.maxHeight = "".concat(currentPlan.scrollHeight, "px");
     currentPlan.style.marginTop = '40px';
+    switcherActiveText = switchers[i].value;
   });
 };
 
@@ -107,12 +109,12 @@ scrollToReaders.addEventListener('click', function () {
 })
 
 
-// popup closer
+// popups
 
 let thanksCloser = document.querySelectorAll('._thanks-closer');
 let thanks = document.querySelector('.thanks');
 
-var _loop3 = function _loop(i) {
+var _loop3 = function _loop8(i) {
   thanksCloser[i].addEventListener('click', function (event) {
     if (event.target === thanksCloser[i]) {
       thanks.classList.remove('active');
@@ -124,6 +126,34 @@ for (var i = 0; i < thanksCloser.length; i++) {
   _loop3(i);
 }
 
+let popupPlanCloser = document.querySelectorAll('._popup-plan-closer');
+let popupPlanOpener = document.querySelectorAll('._popup-plan-opener');
+let popupPlan = document.querySelector('.popup-plan');
+
+var _loop7 = function _loop9(i) {
+  popupPlanOpener[i].addEventListener('click', function (event) {
+    popupPlan.querySelector('#switcherValue').innerHTML = switcherActiveText;
+    popupPlan.querySelector('#planName').innerHTML = event.target.dataset.title;
+    popupPlan.classList.add('active');
+  });
+};
+
+for (var i = 0; i < popupPlanOpener.length; i++) {
+  _loop7(i);
+}
+
+var _loop6 = function _loop10(i) {
+  popupPlanCloser[i].addEventListener('click', function (event) {
+    if (event.target === popupPlanCloser[i]) {
+      popupPlan.classList.remove('active');
+    }
+  });
+};
+
+for (var i = 0; i < popupPlanCloser.length; i++) {
+  _loop6(i);
+}
+
 //validation
 
 function isValid(input) {
@@ -131,8 +161,8 @@ function isValid(input) {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(input.value).toLowerCase());
     }
-    if (input.name === 'message') {
-        return !!input.value;
+    if (input.name === 'name' || input.name === 'message' || input.name === 'business' || input.name === 'service') {
+      return !!input.value;
     }
     return true
 }
@@ -182,7 +212,8 @@ var _loop = function _loop(i) {
       /* response.ok */
       true) {
         // let result = await response.json();
-        if (thanks) {
+        if (thanks && popupPlan) {
+          popupPlan.classList.remove('active');
           thanks.classList.add('active');
         } else {
           alert('Thank you for your interest in our company. We will contact you very soon');
